9ca85205aa4ae7a239864424369018a94c599bf4
sairam566 <sairam1431995@gmail.com>
2025-03-22 15:00:50 +0530

    Added React-Bootstrap dependency

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
 
 function Header() {
   return (
     <div>
+      <h1>
+        Example heading
+        <Badge bg="secondary" as={Button}>
+          New
+        </Badge>
+      </h1>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Header from "./Components/Header.jsx";
+import Header from "./components/Header.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
