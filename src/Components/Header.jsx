871b7a7570c0049d8b0939e2daec7e0611fd661d
sairam566 <sairam1431995@gmail.com>
2025-03-23 13:08:29 +0530

    Added Editor

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,30 +1,45 @@
 import React from "react";
-import { Navbar, Nav, Button, Stack, Container } from 'react-bootstrap';
+import { Navbar, Nav, Button, Stack, Container } from "react-bootstrap";
 import JsonDropDown from "./JsonDropDown.jsx";
 import XmlDropDown from "./XmlDropDown.jsx";
 import Logo from "./Logo.jsx";
 
 function Header() {
   return (
-    <Navbar bg="primary" variant="dark" expand="lg" className="px-3" style={{ minHeight: '50px' }}>
+    <Navbar
+      bg="primary"
+      variant="dark"
+      expand="lg"
+      className="px-3"
+      style={{ minHeight: "50px" }}
+    >
       <Container fluid>
-        <Stack direction="horizontal" gap={3} className="w-100 align-items-center justify-content-between">
+        <Stack
+          direction="horizontal"
+          gap={3}
+          className="w-100 align-items-center justify-content-between"
+        >
           <Stack direction="horizontal" gap={3} className="align-items-center">
             <Navbar.Brand href="#home">
               <Logo />
             </Navbar.Brand>
           </Stack>
-          <Stack direction="horizontal" gap={3}>
-            <Nav.Link href="#home" className="text-light">Home</Nav.Link>
-            <Nav.Link href="#home" className="text-light">Json Formatter</Nav.Link>
-            <Nav.Link href="#home" className="text-light">Xml Formatter</Nav.Link>
+          <Stack direction="horizontal" gap={5}>
+            <Nav.Link href="#home" className="text-light">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#home" className="text-light">
+              Json Formatter
+            </Nav.Link>
+            <Nav.Link href="#home" className="text-light">
+              Xml Formatter
+            </Nav.Link>
             <JsonDropDown />
             <XmlDropDown />
           </Stack>
         </Stack>
       </Container>
     </Navbar>
-
   );
 }
 
