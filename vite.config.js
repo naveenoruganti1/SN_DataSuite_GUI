e24b967f5ad72a2b381d5ec5a8064f23d72faeb0
Naveen Oruganti <naveenoruganti1@gmail.com>
2025-05-06 17:37:09 +0530

    vite config and logo change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '127.0.0.1', // or 'localhost' or your LAN IP
+    port: 3000       // change this to any port you like
+  }
+});
